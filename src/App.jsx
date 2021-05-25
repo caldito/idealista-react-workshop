a92352b4e500ccc4c399b25266defceb408e57a5
Pablo Gómez-Caldito <pgomezcaldito@idealista.com>
2021-05-25 14:09:47 +0200

    Ad inside App

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,6 @@
 import React from 'react';
+import Ad from './components/Ad'
 
-export default () => {};
+export default () => {
+    return <Ad title="My Ad Title" description = 'My Ad description'/>
+};
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 //import Greeting from './components/Greeting'
 import Ad from './components/Ad'
+import App from './App'
 
 //ReactDOM.render(<Greeting message="Hello world"/>,document.querySelector('#root'))
-ReactDOM.render(<Ad title="My Ad Title" description = 'My Ad description'/>,document.querySelector('#root'))
-
+//ReactDOM.render(<Ad title="My Ad Title" description = 'My Ad description'/>,document.querySelector('#root'))
+ReactDOM.render(<App/>,document.querySelector('#root'))
